e3e818f3d3c2a7ab94f72dd227cb5da74311e0a4
Christopher <ggmethos@autistici.org>
2015-05-21 16:26:13 -0400

    minimal textures

--- EightQueensGAsrc/EightQueensGA/EightQueensGA/MainForm.Designer.cs
+++ EightQueensGAsrc/EightQueensGA/EightQueensGA/MainForm.Designer.cs
@@ -226,8 +226,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(815, 498);
+            this.BackColor = System.Drawing.Color.DarkKhaki;
+            this.ClientSize = new System.Drawing.Size(815, 523);
             this.Controls.Add(this.checkProgress);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.board1);
